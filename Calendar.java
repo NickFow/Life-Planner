2eb5c6583370ebc2632e528a1da39e5a38d9e42e
NickFow <NicholisF2017@gmail.com>
2016-05-26 13:01:45 -0400

    Create Calendar.java

---
+++ Calendar.java
@@ -0,0 +1,8 @@
+public class Calendar {
+	//So in here, we're going to create a sick nasty array that I don't really want to make but oh well...
+	
+	/* So the array needs 12 months, in each month I need the size 
+	   to be 29, 30, or 31 for each day, then inside each one of those, 
+	   then the ability to store a event in any of them.*/
+	
+}
